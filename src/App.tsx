2f2422f472a4d111a1d539cb7ea2461bf47238cf
Daniel Martins <127330986+daniel-martins03@users.noreply.github.com>
2025-02-24 19:42:29 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,24 @@
-// Importa as funções necessárias para testar componentes React
-import { render, screen } from '@testing-library/react';
-// Importa o componente App para teste
-import App from './App';
+// Importa o componente Post para ser utilizado neste arquivo
+import Post from "./components/Post";
 
-// Inicia uma suíte de testes para o componente App
-describe('Test <App />', () => {
-    // Define um caso de teste individual
-    it('Expect render correctly', () => {
-        // Renderiza o componente App
-        render(
-            <App />
-        );
-        // Verifica se um elemento com o atributo 'data-testid' igual a 'comment-button' está presente no documento
-        expect(screen.getByTestId('comment-button')).toBeInTheDocument();
-    });
-})
+// Importa os estilos definidos no arquivo App.module.css
+import styles from './App.module.css';
+
+// Define o componente App como uma função
+function App() {
+  return (
+    // Usa a classe CSS "app" do arquivo App.module.css para estilizar o div
+    <div className={styles.app}>
+      {/*
+        Renderiza o componente Post passando a URL da imagem do Batmóvel
+        e o texto que descreve a imagem
+      */}
+      <Post imageUrl="https://raw.githubusercontent.com/glauccoslima/servidor_estaticos/main/Batm%C3%B3vel.jpg">
+        Olha só que legal minha miniatura do Batmóvel.
+      </Post>
+    </div>
+  );
+}
+
+// Exporta o componente App para ser usado em outros lugares do projeto
+export default App;
